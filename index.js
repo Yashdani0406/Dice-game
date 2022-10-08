c58e8a22eda74c8ca24259bc30f92e1043f08bbf
Yash <112421975+Yashdani0406@users.noreply.github.com>
2022-10-09 01:23:43 +0530

    changed src of images

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1 = Math.floor((Math.random() * 6) + 1);
-var randomsrc1 = "images/dice" + randomNumber1 + ".png";
+var randomsrc1 = "dice" + randomNumber1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", randomsrc1);
 
 var randomNumber2 = Math.floor((Math.random() * 6) + 1);
-var randomsrc2 = "images/dice" + randomNumber2 + ".png";
+var randomsrc2 = "dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomsrc2);
 
 
